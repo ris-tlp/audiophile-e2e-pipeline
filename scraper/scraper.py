bad0db4d8bc5c90ffeec6db6b499fa73a96e6a2c
ris-tlp <omarkhantlp@gmail.com>
2022-10-01 04:52:24 +0300

    encapsulate scraper logic into class, model conversion

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,3 +1,4 @@
+import pprint
 import logging
 import requests
 from bs4 import BeautifulSoup
@@ -9,39 +10,71 @@ with open("logs.log", "w"):
     pass
 
 
-device_type = ["iems", "headphones"]
+class Scraper:
+    """
+    Encapsulates all the logic for the web scraper.
+    """
 
-for device in device_type:
-    url = f"https://crinacle.com/rankings/{device}/"
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-    except requests.exceptions.RequestException as e:
-        logging.log(e)
+    def __init__(self) -> None:
+        self.base_url = "https://crinacle.com/rankings/"
 
-    # Find all tables within page, in this case, the only one
-    iem_table = soup.findChildren("table")[0]
+    def convert_to_model(self, device_data: list[dict], device_type: str) -> list:
+        """Converts list of dictionary to their respective list of model type
 
-    # Get all the headers for the table
-    thead = iem_table.find_all("thead", recursive=False)
-    headers = thead[0].findChildren("th")
-    headers = [cell.text for cell in headers]
+        Args:
+            device_data (list[dict]): List of dictionaries containing each device
+            device_type (str): String specifiying the type of device: headphones or iems
 
-    # Get all rows within the table (excluding links)
-    tbody = iem_table.find_all("tbody", recursive=False)
-    rows = tbody[0].find_all("tr", recursive=False)
+        Returns:
+            list: List containing the dictionaries converted to the specific model type
+        """
+        if device_type == "headphones":
+            converted_data = [Headphone(device) for device in device_data]
+        else:
+            converted_data = [InEarMonitor(device) for device in device_data]
 
-    device_data = []
+        return converted_data
 
-    for row in rows:
-        row_data = {}
-        for i, cell in enumerate(row.find_all("td", recursive=False)):
-            row_data[headers[i]] = cell.get_text()
-        device_data.append(row_data)
+    def scrape(self, device_type: str) -> list[dict]:
+        """
+        Scrapes Crinacle's databases containing technical information about Headphones and IEMs.
 
-    if device == "headphones":
-        headphone = Headphone(device_data[0])
-        print(headphone)
-    else:
-        iem = InEarMonitor(device_data[0])
-        print(iem)
+        Args:
+            device_type (str): specifies the device type and url to be scraped.
+        """
+        url = self.base_url + device_type
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+        except requests.exceptions.RequestException as e:
+            logging.log(e)
+
+        # Find all tables within page, in this case, the only one
+        data_table = soup.findChildren("table")[0]
+
+        # Get all the headers for the table
+        thead = data_table.find_all("thead", recursive=False)
+        headers = thead[0].findChildren("th")
+        headers = [cell.text for cell in headers]
+
+        # Get all rows within the table (excluding links)
+        tbody = data_table.find_all("tbody", recursive=False)
+        rows = tbody[0].find_all("tr", recursive=False)
+
+        device_data = []
+
+        for row in rows:
+            row_data = {}
+            for i, cell in enumerate(row.find_all("td", recursive=False)):
+                row_data[headers[i]] = cell.get_text()
+            device_data.append(row_data)
+
+        device_data = self.convert_to_model(device_data=device_data, device_type=device_type)
+        return device_data
+
+
+if __name__ == "__main__":
+    scraper = Scraper()
+    headphones = scraper.scrape(device_type="iems")
+    pprint.pprint(headphones)
+    # scraper.scrape(device_type="iems")

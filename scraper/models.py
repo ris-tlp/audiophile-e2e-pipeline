ff3a4cf54a674270b8f345fef0ae7bf374255b3b
ris-tlp <omarkhantlp@gmail.com>
2022-10-01 03:40:52 +0300

    scraper mvp for headphones and iems

--- scraper/models.py
+++ scraper/models.py
@@ -1,20 +1,45 @@
 from dataclasses import dataclass
 
-@dataclass
-class _BaseDevice:
+
+@dataclass(init=False)
+class InEarMonitor:
+    """
+    Dataclass containing attributes for InEarMonitors.
+    """
+
     rank: str
     model: str
-    setup: str
     signature: str
     tone_grade: str
+    driver_type: str
     value_rating: str
     technical_grade: str
     price: int
 
-@dataclass
-class IEM(_BaseDevice):
-    type: str = "IEM"
+    def __init__(self, iem_data: dict) -> None:
+        if "Setup" in iem_data:
+            self.driver_type = iem_data["Setup"]
 
-@dataclass
-class Headphone(_BaseDevice):
-    type: str = "Headphone"
+        self.rank = iem_data["Rank"]
+        self.model = iem_data["Model"]
+        self.signature = iem_data["Signature"]
+        self.tone_grade = iem_data["Tone Grade"]
+        self.value_rating = iem_data["Value Rating"]
+        self.technical_grade = iem_data["Technical Grade"]
+        self.price = iem_data["Price (MSRP)"]
+
+
+@dataclass(init=False)
+class Headphone(InEarMonitor):
+    """Dataclass containing attributes for Headphones.
+
+    Args:
+        InEarMonitor (InEarMonitor): subclasses the InEarMonitor class
+    """
+
+    cup_type: str
+
+    def __init__(self, headphone_data: dict) -> None:
+        super().__init__(headphone_data)
+        self.driver_type = headphone_data["Driver Type"]
+        self.cup_type = headphone_data["Fit/Cup Type"]
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,7 @@
 import logging
 import requests
 from bs4 import BeautifulSoup
+from models import Headphone, InEarMonitor
 
 logging.basicConfig(filename="logs.log", level=logging.INFO)
 # truncating log file before new run
@@ -8,27 +9,39 @@ with open("logs.log", "w"):
     pass
 
 
-url = "https://crinacle.com/rankings/iems/"
-try:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-except requests.exceptions.RequestException as e:
-    logging.log(e)
+device_type = ["iems", "headphones"]
 
-# Find all tables within page, in this case, the only one
-iem_table = soup.findChildren("table")[0]
+for device in device_type:
+    url = f"https://crinacle.com/rankings/{device}/"
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+    except requests.exceptions.RequestException as e:
+        logging.log(e)
 
-# Get all the headers for the table
-thead = iem_table.find_all("thead", recursive=False)
-header = thead[0].findChildren("th")
-for cell in header:
-    print(cell.text)
+    # Find all tables within page, in this case, the only one
+    iem_table = soup.findChildren("table")[0]
 
-# Get all rows within the table (excluding links)
-tbody = iem_table.find_all("tbody", recursive=False)
-rows = tbody[0].find_all("tr", recursive=False)
+    # Get all the headers for the table
+    thead = iem_table.find_all("thead", recursive=False)
+    headers = thead[0].findChildren("th")
+    headers = [cell.text for cell in headers]
 
-for row in rows:
-    for cell in row:
-        print(cell.text)
-    break
+    # Get all rows within the table (excluding links)
+    tbody = iem_table.find_all("tbody", recursive=False)
+    rows = tbody[0].find_all("tr", recursive=False)
+
+    device_data = []
+
+    for row in rows:
+        row_data = {}
+        for i, cell in enumerate(row.find_all("td", recursive=False)):
+            row_data[headers[i]] = cell.get_text()
+        device_data.append(row_data)
+
+    if device == "headphones":
+        headphone = Headphone(device_data[0])
+        print(headphone)
+    else:
+        iem = InEarMonitor(device_data[0])
+        print(iem)

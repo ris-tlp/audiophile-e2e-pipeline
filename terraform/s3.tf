aab95dec956e57cee7e6399a6498bc4463e4f086
ris-tlp <omarkhantlp@gmail.com>
2022-10-01 13:42:39 +0300

    refactor

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,10 @@
+# Create S3 bucket with a specific prefix
 resource "aws_s3_bucket" "audiophile-bucket" {
   bucket_prefix = var.bucket_prefix
   force_destroy = true
 }
 
+# Enable versioning for S3 bucket
 resource "aws_s3_bucket_versioning" "audiophile-bucket-versioning" {
   bucket = aws_s3_bucket.audiophile-bucket.id
   versioning_configuration {

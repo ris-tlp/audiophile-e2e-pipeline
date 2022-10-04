dc7ea9221a226d4a6a20f0dd870565ed5e07a0be
ris-tlp <omarkhantlp@gmail.com>
2022-10-04 05:07:33 +0300

    add shell script to generate aws/terraform config

--- generate_config.sh
+++ generate_config.sh
@@ -1,8 +1,2 @@
-# cd /opt/terraform/
-# touch /opt/airflow/tasks/configuration.env
-# terraform output >> /opt/airflow/tasks/configuration.env
-# cat /opt/airflow/tasks/configuration.envs
-
-touch #airflow/tasks/configuration.env
 cd terraform/
 terraform output > ../airflow/tasks/configuration.env

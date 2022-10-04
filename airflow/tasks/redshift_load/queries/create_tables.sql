ca02db1a839c9cf6d1c148e22129eb85726902c9
ris-tlp <omarkhantlp@gmail.com>
2022-10-04 20:41:17 +0300

    finalize table creation

---
+++ airflow/tasks/redshift_load/queries/create_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS Headphones (
+    id int PRIMARY KEY,
+    rank_grade varchar(2) NOT NULL,
+    model varchar(50) NOT NULL,
+    value_rating int NOT NULL,
+    price int NOT NULL,
+    audio_signature varchar(50) NOT NULL,
+    tone_grade varchar(2) NOT NULL,
+    technical_grade varchar(2) NOT NULL,
+    driver_type varchar(25) NOT NULL,
+    fit_cup varchar(25) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS InEarMonitor (
+    id int PRIMARY KEY,
+    rank_grade varchar(2) NOT NULL,
+    model varchar(50) NOT NULL,
+    value_rating int NOT NULL,
+    price int NOT NULL,
+    audio_signature varchar(50) NOT NULL,
+    tone_grade varchar(2) NOT NULL,
+    technical_grade varchar(2) NOT NULL,
+    driver_type varchar(25) NOT NULL
+);
--- airflow/tasks/redshift_load/upload_to_redshift.py
+++ airflow/tasks/redshift_load/upload_to_redshift.py
@@ -1,16 +1,15 @@
 import pathlib
 import psycopg2
-from dotenv import dotenv_values
 from psycopg2 import sql
+from dotenv import dotenv_values
 
 # Load config
 configuration_path = pathlib.Path(__file__).parent.parent.resolve()
 script_path = pathlib.Path(__file__).parent.resolve()
-# config = dotenv_values("opt/airflow/tasks/configuration.env")
 config = dotenv_values(f"{configuration_path}/configuration.env")
 
 
-def create_conn(config):
+def create_conn():
     try:
         conn = psycopg2.connect(
             host=config["redshift_host"].split(":")[0],
@@ -25,23 +24,29 @@ def create_conn(config):
         print(exception)
 
 
-def prepare_query(query_filename, *args, **kwargs):
+def prepare_query(query_filename: str) -> str:
+    """
+    Helper method to concatenate entire query and help with preparing parameters
+
+    Args:
+        query_filename (str): name of the sql file containing query
+
+    Returns:
+        str: completely concatenated query
+    """
     query_cursor = open(f"{script_path}/queries/{query_filename}.sql", "r")
     query = query_cursor.read()
-    query = query.strip()
-
-    # query = sql.SQL(query).format(table_name=sql.Identifier("Headphones"))
-    query = query.format(table_name="Headphones")
-    print(query)
 
     return query
 
 
 if __name__ == "__main__":
-    conn = create_conn(config)
+    conn = create_conn()
     cursor = conn.cursor()
-    q = prepare_query("create")
 
-    cursor.execute(q)
+    # Creating tables
+    create_tables_query = prepare_query("create_tables")
+
+    cursor.execute(create_tables_query)
     conn.commit()
 

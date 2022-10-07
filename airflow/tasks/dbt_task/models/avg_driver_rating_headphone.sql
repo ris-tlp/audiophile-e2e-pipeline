507f9ca30c8ea60aab7a52d3adb4bb7bbe975c44
ris-tlp <omarkhantlp@gmail.com>
2022-10-07 18:18:44 +0300

    average rating of headphone driver types

---
+++ airflow/tasks/dbt_task/models/avg_driver_rating_headphone.sql
@@ -0,0 +1,23 @@
+WITH mapped_ranks AS (
+    SELECT
+        *
+    FROM
+        {{ ref("stg_maprankvalues") }}
+),
+final AS (
+    SELECT
+        headphone.driver_type,
+        AVG(mapped_ranks.rank_value) AS average_rating,
+        COUNT(headphone.driver_type) AS number_of_devices
+    FROM
+        headphone,
+        mapped_ranks
+    WHERE
+        headphone.rank_grade = mapped_ranks.rank_grade
+    GROUP BY
+        headphone.driver_type
+)
+SELECT
+    *
+FROM
+    final

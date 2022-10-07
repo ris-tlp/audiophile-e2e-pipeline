cf7bd2c55aded308a32a264e066268b63907387b
ris-tlp <omarkhantlp@gmail.com>
2022-10-07 18:18:34 +0300

    fix jinja namespacing issue

--- airflow/tasks/dbt_task/models/staging/stg_maprankvalues.sql
+++ airflow/tasks/dbt_task/models/staging/stg_maprankvalues.sql
@@ -2,30 +2,30 @@
 -- assigns a numerical value to each rank.
 
 {% set rank_grades = ["S+", "S", "S-", "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "E+", "E", "E-", "F+", "F", "F-"] %}
-{% set value = 10 %}
+{% set ns = namespace(value=10) %}
 
 WITH iem_ranks AS (
     SELECT DISTINCT
         rank_grade,
         CASE
             {% for grade in rank_grades %}
-            WHEN rank_grade = '{{ grade }}' THEN {{ value }}
-            {% set value = value - 0.5 %}
+            WHEN rank_grade = '{{ grade }}' THEN {{ ns.value }}
+            {% set ns.value = ns.value + 0.5 %}
             {% endfor %}
         END AS rank_value
     FROM
         inearmonitor
 ),
 
-{% set value = 10 %}
+{% set ns = namespace(value=10) %}
 
 headphone_ranks AS (
     SELECT DISTINCT
         rank_grade,
         CASE
             {% for grade in rank_grades %}
-            WHEN rank_grade = '{{ grade }}' THEN {{ value }}
-            {% set value = value - 0.5 %}
+            WHEN rank_grade = '{{ grade }}' THEN {{ ns.value }}
+            {% set ns.value = ns.value + 0.5 %}
             {% endfor %}
         END AS rank_value
     FROM
